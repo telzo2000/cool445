1dd882904071a70284f9d13d0a9d3d52223ec980
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2023-10-09 16:59:28 +0900

    fix

---
+++ QMK_firmwqare(22.3)/cool445rp/keymaps/via/keymap.c
@@ -0,0 +1,13 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    
+    [0] = LAYOUT_ortho_4x12(
+             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,               KC_U,     KC_I,     KC_O,    KC_P,    KC_BSPC, 
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,               KC_J,     KC_K,     KC_L,    KC_MINUS,KC_ENT, 
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,               KC_M,     KC_COMM,  KC_DOT,  KC_UP,   KC_LSFT,
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPACE,KC_SPACE, KC_SPC,    KC_LEFT, KC_DOWN, KC_RIGHT              
+  )
+  
+};
+
