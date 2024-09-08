265e8a212fac7e4c9695b7418742b1690ecc5a6a
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2024-09-09 06:12:50 +0900

    fix

---
+++ QMK_firmwqare(22.3)/cool445/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+// Copyright 2023 m.ki (@telzo2000)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0xDE, 0x7C, 0x4F, 0x84, 0xBB, 0x71, 0x09, 0xBD}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
--- QMK_firmwqare(22.3)/cool445/rules.mk
+++ QMK_firmwqare(22.3)/cool445/rules.mk
@@ -1,2 +1,19 @@
+# This file intentionally left blank
+NKRO_ENABLE = no            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
+ENCODER_ENABLE = no
+POINTING_DEVICE_ENABLE = no
+OLED_ENABLE = no
 
-SPLIT_KEYBOARD = no
+
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+MUSIC_ENABLE = no
+
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = no
